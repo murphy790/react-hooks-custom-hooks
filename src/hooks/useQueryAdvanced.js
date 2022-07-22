cf2f81b72359241d8f93061878dbef121fa499a9
Alex Mwaura <alexmwaura36@gmail.com>
2022-07-22 14:35:25 +0300

    Done.

--- src/hooks/useQueryAdvanced.js
+++ src/hooks/useQueryAdvanced.js
@@ -12,12 +12,8 @@ function createAbortableFetch() {
   };
 }
 
-// we'll use this object to *memoize* the results from calling fetch
-// ideally, this wouldn't be a variable outside of the hook
-// (could use context and a ref instead as a way of sharing this data between components)
 const cache = {};
 
-// reducer function for our fetch state
 function fetchReducer(state, action) {
   switch (action.type) {
     case "pending":
@@ -31,10 +27,8 @@ function fetchReducer(state, action) {
   }
 }
 
-// here's a more optimized version of the useQuery hook
 function useQuery(url, initialData = null) {
-  // using a reducer helps ensure consistent state
-  // and prevents us from getting into 'impossible' states
+  
   const [{ status, data, error }, dispatch] = useReducer(fetchReducer, {
     status: "idle",
     data: initialData,
@@ -43,38 +37,32 @@ function useQuery(url, initialData = null) {
 
   useEffect(() => {
     if (cache[url]) {
-      // if the result is cached, we can return it and show it immediately to the user
+      
       dispatch({ type: "resolved", payload: cache[url] });
     } else {
-      // otherwise, show them the pending state while we fetched
+      
       dispatch({ type: "pending" });
     }
 
-    // even if the result is cached on the client's machine, might be a good idea
-    // to get the fresh data from the server, in case there were updates on the
-    // server after the user cached the initial response
-
-    // this is what we'll use to cancel the fetch if the component un-mounts before fetch resolves
     const { fetch, abort } = createAbortableFetch();
 
     fetch(url)
       .then((r) => r.json())
       .then((data) => {
-        // save response to cache
+      
         cache[url] = data;
-        // dispatch the saved data
+      
         dispatch({ type: "resolved", payload: data });
       })
       .catch((err) => {
-        // AbortError will be thrown if we cancel the fetch request
+  
         if (err.name !== "AbortError") {
-          // for other errors, we can dispatch them so they're set into state
           dispatch({ type: "rejected", payload: err });
         }
       });
 
     return function () {
-      // cancel the fetch request if the component un-mounts
+  
       abort();
     };
   }, [url]);
